0b17f1290d3e3c10ef072e062c61f837eae29ce1
Emilien Mantel <hanx@emilien.(none)>
2013-10-24 17:46:40 +0200

    Same version syntax everywhere

--- pod/create-manpages.pl
+++ pod/create-manpages.pl
@@ -4,17 +4,15 @@ use strict;
 use warnings;
 
 use feature qw/say/;
-use Data::Dumper;
 
 use constant {
-	'RELEASE' => '1.0git',
+	'RELEASE' => '1.0-git',
 	'CENTER' => '""'
 };
 
 foreach my $file (glob('*.pod'))
 {
 	my ($locale, $section) = ($file =~ /^se_([a-z]{2})\.(\d)\.pod$/);	
-	print Dumper ($locale, $section);
 	my $outfile = 'se_'.$locale.'.'.$section.'.man';
 	my $command = 'pod2man --release '.RELEASE.' --section '.$section.' --center '.CENTER.' -n se '.$file.' ../man/'.$outfile;
 	say $command;
