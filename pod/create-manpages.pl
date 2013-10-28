cf40f1aecf14e5736420a040afb9d35e3f6228ba
Emilien Mantel <hanx@emilien>
2013-10-28 14:42:43 +0100

    version number in in VERSION file

--- pod/create-manpages.pl
+++ pod/create-manpages.pl
@@ -6,15 +6,20 @@ use warnings;
 use feature qw/say/;
 
 use constant {
-	'RELEASE' => '1.0-git',
 	'CENTER' => '""'
 };
+
+open('VERSION', '<', '../VERSION');
+my $release = <VERSION>;
+chomp $release;
+close(VERSION);
+
 
 foreach my $file (glob('*.pod'))
 {
 	my ($locale, $section) = ($file =~ /^se_([a-z]{2})\.(\d)\.pod$/);	
 	my $outfile = 'se_'.$locale.'.'.$section.'.man';
-	my $command = 'pod2man --release '.RELEASE.' --section '.$section.' --center '.CENTER.' -n se '.$file.' ../man/'.$outfile;
+	my $command = 'pod2man --release "'.$release.'" --section '.$section.' --center '.CENTER.' -n se '.$file.' ../man/'.$outfile;
 	say $command;
 	`$command`;
 }
