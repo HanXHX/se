9710448ad5aae1ec523d0caba55512666ea9b16d
Emilien Mantel <hanx@emilien>
2013-11-08 10:00:04 +0100

    new doc generation - first step

---
+++ doc/create-manpages.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use feature qw/say/;
+
+use constant {
+	'VERSION_FILE' => '../VERSION'
+};
+
+open('VERSION', '<', VERSION_FILE) or die('Can\'t open ' . VERSION_FILE);
+my $release = <VERSION>;
+chomp $release;
+close(VERSION);
+
+
+foreach my $file (glob('pod/*.pod'))
+{
+	my ($locale, $section) = ($file =~ /se_([a-z]{2})\.(\d)\.pod$/);	
+
+	my $outfolder = sprintf(
+		'./man/%sman%d',
+		$locale ne 'en' ? $locale . '/' : '', $section
+	);
+
+	mkdir($outfolder) or die("Cant't mkdir $outfolder");
+
+	my $outfile = sprintf(
+		'%s/se.%d',
+		$outfolder, $section
+	);
+
+	my $command = sprintf(
+		qq/pod2man --release "%s" --section "%d" --center "" -n se %s %s/,
+		$release, $section, $file, $outfile
+	);
+
+	say $command;
+	`$command`;
+}
